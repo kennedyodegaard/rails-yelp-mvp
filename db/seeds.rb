77747c6047df1fd336eeb1a20624889bf4f76517
Kennedy Odegaard <kennedyaodegaard@gmail.com>
2020-11-25 16:17:10 +0100

    this is functional and we can write reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+require 'faker'
+p "About to give you 5 fake restaurants..."
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    )
+end
+p "and we're done...."
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
